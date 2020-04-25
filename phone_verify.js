df0d212146407c25e94254d02a734022810f456c
SaifRehman99 <saifr7493@gmail.com>
2020-04-25 15:16:38 +0500

    phone number display

--- phone_verify.js
+++ phone_verify.js
@@ -2,9 +2,6 @@ window.onload = function() {
     render();
 };
 
-
-
-
 function render() {
     // window.recaptchaVerifier = new firebase.auth.RecaptchaVerifier('recaptcha-container', {
     // 'size': 'invisible',
@@ -14,36 +11,46 @@ function render() {
     //phoneAuth()
     //}
     //});
-    window.recaptchaVerifier = new firebase.auth.RecaptchaVerifier('recaptcha-container');
+    window.recaptchaVerifier = new firebase.auth.RecaptchaVerifier(
+        "recaptcha-container"
+    );
     recaptchaVerifier.render();
 }
 
 function phoneAuth() {
     //get the number
-    var number = document.getElementById('number').value;
+    var number = document.getElementById("number").value;
     //phone number authentication function of firebase
     //it takes two parameter first one is number,,,second one is recaptcha
-    firebase.auth().signInWithPhoneNumber(number, window.recaptchaVerifier).then(function(confirmationResult) {
-        //s is in lowercase
-        window.confirmationResult = confirmationResult;
-        coderesult = confirmationResult;
-        console.log(coderesult);
-        alert('Message sent to your phone');
-    }).catch(function(error) {
-        alert(error.message);
-    });
+    firebase
+        .auth()
+        .signInWithPhoneNumber(number, window.recaptchaVerifier)
+        .then(function(confirmationResult) {
+            //s is in lowercase
+            window.confirmationResult = confirmationResult;
+            coderesult = confirmationResult;
+            console.log(coderesult);
+            alert("Message sent to your phone");
+        })
+        .catch(function(error) {
+            alert(error.message);
+        });
 }
 
 function codeverify() {
-    var code = document.getElementById('verificationCode').value;
-    coderesult.confirm(code).then(function(result) {
-        if (result.user.uid) {
-            window.location = 'register.html'
-        } else {
-            window.location = 'phone_verify.html'
-        }
-
-    }).catch(function(error) {
-        alert(error.message);
-    });
+    var code = document.getElementById("verificationCode").value;
+    coderesult
+        .confirm(code)
+        .then(function(result) {
+            if (result.user.uid) {
+                let phoneNumber = result.user.phoneNumber;
+                window.location = "register.html";
+                console.log(user);
+            } else {
+                window.location = "phone_verify.html";
+            }
+        })
+        .catch(function(error) {
+            alert(error.message);
+        });
 }
